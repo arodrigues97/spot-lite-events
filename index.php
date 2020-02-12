8a054e0d0e495db91f6b6bc9f681ab2f824903e9
Adam Rodrigues <36482887+arodrigues97@users.noreply.github.com>
2020-02-12 06:19:14 -0500

    Home page and main styling done

--- index.php
+++ index.php
@@ -1,63 +1,159 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>
-    
-    </title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
-  </head>
-  <body>
-      
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<head>
+  <title>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+  </title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="/css/main.css">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-dark navbar-expand-md">
+    <a class="navbar-brand" href="#"><img src="/images/logo.png"></a>
+    <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
+      <ul class="navbar-nav mx-auto text-center">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <div class="active">Home <span class="sr-only">Home</span></div>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="//codeply.com">Packages</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav flex-row justify-content-center flex-nowrap">
+        <li class="nav-item"><a class="nav-link" href=""><i class="fa fa-facebook mr-1"></i></a> </li>
+        <li class="nav-item"><a class="nav-link" href=""><i class="fa fa-twitter"></i></a> </li>
+      </ul>
+    </div>
+  </nav>
+
+
+  <header class="masthead">
+
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-12 text-center">
+          <h1 class="">SpotLite Events</h1>
+          <p class="">Servicing Our Effects For Your Special Day</p>
+
+          <button class="btn btn-info">Book Now</button>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+      </div>
+    </div>
+  </header>
+
+  <div class="container-fluid">
+    <div class="services">
+      <h1 id="servicesH1">Our Services</h1>
+      <div class="row">
+
+        <div class="col bar-right">
+          <div class="service">
+            <h1>Cold Spark Fountain</h1>
+            <p>Our cold spark machines are a perfect choice for enhancing the experiences of first dances, new product launches, shows, grand entrances and various other types of events. Cold spark machines can either be fired all together, in a programmed sequence and/or one by one in order to create beautiful effects and create waves.</p>
+          </div>
+        </div>
+        <div class="col bar-left">
+          <div class="service">
+            <h1>Dry Fog Machine</h1>
+            <p>Dry ice fog machines are utilized to produce a thick fog effect that enhances lighting and the mood of an event.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="learn-more">
+        <button class="btn btn-info my-2">Learn More</button>
+      </div>
+    </div>
   </div>
-</nav>
+
+  <div class="container-fluid testimonial">
+    <div class="row">
+      <div class="col left"> </div>
+      <div class="col right">
+        <p>Working with the SpotLite team was a fantastic experience. They went above and beyond at my wedding and really set the tone for an amazing night.</p>
+        <span>Hannah & Jan</span>
+      </div>
+    </div>
+  </div>
 
 
-<div class="jumbotron jumbotron-fluid">
+  <div class="container-fluid">
+    <div class="logo">
+      <img src="/images/logo.png" alt="">
+    </div>
+  </div>
 
-</div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+  <div class="container-fluid">
+
+    <div class="row offers">
+      <div class="col bar-right">
+        <h1>High Quality Products</h1>
+        <p>Our machines are 100% safe and use no fire,flame or dangerous chemicals. The sparks are completely harmless to run your fingers through and pose no hazard to any venue. Depending on your preference, our machines can be fired simultaneously or in a programmed sequence. We guarantee our products will leave attendees engaged and excited.</p>
+      </div>
+      <div class="col ">
+        <h1>Capturing Your Special Moments</h1>
+        <p>From grand entrances, to the intimacy of first dances, our product will guarantee a picture perfect moment you can share with your loved ones. With the range of usage being endless, we challenge you to spark up any event worthy of a candid.</p>
+      </div>
+      <div class="col bar-left">
+        <h1>Transparent Pricing</h1>
+        <p>In comparison to our competitors, we guarantee budget-friendly pricing that won't break the bank. Our team does a fantastic job of tailoring to your situation and providing a price-point that is cheaper than the competition.</p>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+
+    <div class="container-fluid">
+
+      <div class="row">
+
+        <div class="col bar-right">
+          <span>Working with people all over Ontario, Canada.</span>
+        </div>
+        <div class="col">
+          <span>Tel: 519 241 6088</span>
+          <span><br>inquiryspotliteevents@gmail.com</span>
+        </div>
+
+        <div class="col bar-left">
+          <span><br>&copy; 2023 by SpotLite Events.
+        </div>
+
+        <div class="col">
+          <img src="/images/linkedin.png">
+          <img src="/images/facebook.png">
+          <img src="/images/instagram.png">
+          <spa><br>@SpoteLiteEvents</span>
+        </div>
+
+      </div>
+
+
+  </footer>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
 </html>
